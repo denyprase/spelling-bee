72e8b51446a02726d8726d4d6fcba32a537740ed
deny <deny1892@gmail.com>
2025-04-07 22:32:19 +0700

    try variable in script

--- handlers/spelling.go
+++ handlers/spelling.go
@@ -2,9 +2,15 @@ package handlers
 
 import (
 	"net/http"
+	"spelling-bee/models"
 	"spelling-bee/utils"
 )
 
 func SpellingHandler(w http.ResponseWriter, r *http.Request) {
-	utils.RenderTemplate(w, r, "spelling.html", map[string]interface{}{})
+	ses, _ := models.GetSessionByID("1")
+
+	utils.RenderTemplate(w, r, "spelling.html", map[string]interface{}{
+		"CountdownTime":   ses.AnswerTime,
+		"DisplayDuration": ses.DisplayTime * 1000,
+	})
 }
